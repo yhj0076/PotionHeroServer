bb2326775aba14956853325ff87518ffc6a6f9c2
King0wl <70518135+yhj0076@users.noreply.github.com>
2025-04-14 02:21:29 +0900

    chore: 사용하지 않는 Program 삭제

--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
-﻿namespace PotionHeroServer;
+﻿using ServerCore;
+
+namespace PotionHeroServer;
 
 class Program
 {
+    static Listener _listener = new Listener();
+    
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
