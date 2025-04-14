241b85745aa44a4bbff65d26c1cee047cc193d35
King0wl <70518135+yhj0076@users.noreply.github.com>
2025-04-14 17:54:00 +0900

    feat: clientPacket 생성 완료

---
+++ Packet/ClientPacket.cs
@@ -0,0 +1,64 @@
+﻿using ServerCore;
+
+namespace PotionHeroServer.Packet;
+
+public enum PacketType
+{
+    S_BroadcastGainedDmg = 1,
+    S_BroadcastAttack = 2,
+    C_GainedDmg = 3,
+    S_BroadcastTime = 4,
+}
+
+public interface IPacket
+{
+    ushort Protocol { get; }
+    void Read(ArraySegment<byte> segment);
+    ArraySegment<byte> Write();
+}
+
+public class S_BroadcastGainedDmg : IPacket
+{
+    public int hostGainedDmg { get; set; }
+    public int guestGainedDmg { get; set; }
+    
+    public ushort Protocol { get {return (ushort)PacketType.S_BroadcastGainedDmg; } }
+    
+    public void Read(ArraySegment<byte> segment)
+    {
+        ushort count = 0;
+        ReadOnlySpan<byte> buffer = new ReadOnlySpan<byte>(segment.Array, segment.Offset, segment.Count);
+        count += sizeof(ushort);
+        count += sizeof(ushort);
+        this.hostGainedDmg = BitConverter.ToInt32(buffer.Slice(count, buffer.Length - count));
+        count += sizeof(int);
+        this.guestGainedDmg = BitConverter.ToInt32(buffer.Slice(count, buffer.Length - count));
+        count += sizeof(int);
+    }
+
+    public ArraySegment<byte> Write()
+    {
+        ArraySegment<byte> segment = SendBufferHelper.Open(8192);
+
+        ushort count = 0;
+        bool success = true;
+
+        Span<byte> buffer = new Span<byte>(segment.Array, segment.Offset, segment.Count);
+        
+        count += sizeof(ushort);
+        success &= BitConverter.TryWriteBytes(buffer.Slice(count,buffer.Length - count), (ushort)PacketType.S_BroadcastGainedDmg);
+        count += sizeof(ushort);
+        
+        success &= BitConverter.TryWriteBytes(buffer.Slice(count,buffer.Length - count), hostGainedDmg);
+        count += sizeof(int);
+        success &= BitConverter.TryWriteBytes(buffer.Slice(count,buffer.Length - count), guestGainedDmg);
+        count += sizeof(int);
+        
+        success &= BitConverter.TryWriteBytes(buffer, count);
+        
+        if (success == false)
+            return null;
+
+        return SendBufferHelper.Close(count);
+    }
+}
