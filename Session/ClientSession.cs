c6e0ae2aaee57c6d002125c0f0999db05e7b499a
King0wl <70518135+yhj0076@users.noreply.github.com>
2025-04-14 17:54:29 +0900

    feat: ClientSession 생성 완료

---
+++ Session/ClientSession.cs
@@ -0,0 +1,35 @@
+﻿using System.Net;
+using ServerCore;
+
+namespace PotionHeroServer.Session;
+
+public class ClientSession : PacketSession
+{
+    public int SessionId { get; set; }
+    public GameRoom Room { get; set; }
+    
+    public int HP { get; set; }
+    public int gainedPower  { get; set; }
+    
+    public override void OnConnected(EndPoint endPoint)
+    {
+        Console.WriteLine($"Connected to {endPoint}");
+        
+        Program.Room.
+    }
+
+    public override void OnSend(int numOfBytes)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void OnDisconnected(EndPoint endPoint)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void OnRecvPacket(ArraySegment<byte> arraySegment)
+    {
+        throw new NotImplementedException();
+    }
+}
